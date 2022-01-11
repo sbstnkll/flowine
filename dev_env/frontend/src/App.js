869d9ce27997a4a4e8376b8d652a3827c9677a77
sbstnkll <sebastian.kulle@gmail.com>
2022-01-11 15:19:58 +0100

    update

--- dev_env/frontend/src/App.js
+++ dev_env/frontend/src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import flowine from "flowine";
+
 function App() {
+
+
+
   return (
     <div className="App">
       <header className="App-header">
